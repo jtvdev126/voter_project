8186e9d1537b16dea7c6778263c5ad4411b0cbe7
Joel Varughese <jtvdev126@gmail.com>
2023-09-21 15:30:00 -0500

    condensed the variable totals

--- voter.py
+++ voter.py
@@ -1,29 +1,9 @@
 import pandas as pd
 
-# Create voter total to represent all of voting population
-voter_total = 0
-
-# Create democratic voter total
-demo_voter_total = 0
-
-# Create republican voter total
-rep_voter_total = 0
-
-# Create independent voter total
-ind_voter_total = 0
-
-# Create unregistered voter total
-unreg_voter_total = 0
+# Create total to represent all of voting population
+# Create totals for all political parties voters
+voter_total, demo_voter_total, rep_voter_total, ind_voter_total, unreg_voter_total = 0
 
 # Totals for age groups
-# age 18-25
-genz_voter_total = 0
+genz_voter_total, millenial_voter_total, genx_voter_total, boomer_voter_total = 0
 
-# age 26-44
-millenial_voter_total = 0
-
-# age 45-64
-genx_voter_total = 0
-
-# age 65+
-boomer_voter_total = 0

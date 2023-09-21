d6730bcc3a32476b660ae2f895a25c6116238582
Joel Varughese <jtvdev126@gmail.com>
2023-09-21 15:48:51 -0500

    added ethnic, genders, income, edu, geo

--- voter.py
+++ voter.py
@@ -2,8 +2,23 @@ import pandas as pd
 
 # Create total to represent all of voting population
 # Create totals for all political parties voters
-voter_total, demo_voter_total, rep_voter_total, ind_voter_total, unreg_voter_total = 0
+voter_total = 0 
+democratic_voter_total, republican_voter_total, independent_voter_total, unregistered_voter_total = 0
 
 # Totals for age groups
 genz_voter_total, millenial_voter_total, genx_voter_total, boomer_voter_total = 0
 
+# Totals for ethnic groups
+white, black, hispanic, asian, other = 0
+
+# Totals for genders
+male, female, nonbinary = 0
+
+# Totals for income brackets
+poor, middle_class, upper_middle, wealthy = 0
+
+# Totals for education
+no_education, high_school_graduate, college_graduate = 0
+
+# Totals for macro geography
+urban, suburban, rural = 0

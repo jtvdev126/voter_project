0d58891f1fb17418da193894989d51151c8275e3
Joel Varughese <jtvdev126@gmail.com>
2023-09-21 11:52:31 -0500

    added totals for all voters and political parties

---
+++ voter.py
@@ -0,0 +1,14 @@
+# Create voter total to represent all of voting population
+voter_total = 0
+
+# Create democratic voter total
+demo_voter_total = 0
+
+# Create republican voter total
+rep_voter_total = 0
+
+# Create independent voter total
+ind_voter_total = 0
+
+# Create unregistered voter total
+unreg_voter_total = 0

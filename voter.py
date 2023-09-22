b4710322f4b01ece8e57ffab1bb5eb6e771b7b94
Joel Varughese <jtvdev126@gmail.com>
2023-09-22 16:17:11 -0500

    list of strings set to variables

--- voter.py
+++ voter.py
@@ -4,15 +4,19 @@ import pandas as pd
 # Create totals for all political parties voters
 voter_total = 0 
 democratic_voter_total, republican_voter_total, independent_voter_total, unregistered_voter_total = 0
+political_affiliation = ["Democratic", "Republican", "Independent", "Unregistered"]
 
 # Totals for age groups
 genz_voter_total, millenial_voter_total, genx_voter_total, boomer_voter_total = 0
+ages = ["Generation_Z", "Millenial", "Generation_X", "Boomer"]
 
 # Totals for ethnic groups
 white, black, hispanic, asian, other = 0
+races = ["white", "black", "hispanic", "asian", "other"]
 
 # Totals for genders
 male, female, nonbinary = 0
+genders = ["male", "female", "nonbinary"]
 
 # Totals for income brackets
 poor, middle_class, upper_middle, wealthy = 0
@@ -22,3 +26,4 @@ no_education, high_school_graduate, college_graduate = 0
 
 # Totals for macro geography
 urban, suburban, rural = 0
+
